06db70bc2e196c014995734b082d7672cfc0ce36
ParsifalCXX <volodymyrharash@protonmail.com>
2026-06-05 18:43:59 +0300

    renderer update

--- source/Renderer.h
+++ source/Renderer.h
@@ -1,3 +1,16 @@
 #pragma once
+#include "vulkan/vulkan.h"
 
-class Renderer {};
+#include <vulkan/vulkan_core.h>
+
+class Renderer {
+public:
+  virtual bool init() = 0;
+};
+
+class VkRenderer : public Renderer {
+  VkInstance m_instance;
+
+public:
+  virtual bool init() override;
+};
